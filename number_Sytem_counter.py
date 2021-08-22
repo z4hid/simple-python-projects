6ffbd59e0c5cf3d9f9ff8a32d8f15a2d17147f54
zahidhasanshuvo <zahidhasan9297@gmail.com>
2021-08-22 14:41:18 +0600

    First commit

---
+++ number_Sytem_counter.py
@@ -0,0 +1,26 @@
+print("Number System Counter")
+
+while True:
+    try:
+        start = int(input("Enter starting number: "))
+        break
+    except ValueError:
+        print("Please enter an integer value.\n")
+
+
+while True:
+    try:
+        amount = int(input("Enter how many numbers to display: "))
+        break
+    except ValueError:
+        print("Please enter an integer value.\n")
+
+
+for number in range(start, start + amount):
+    # Convert to hexadecimal
+    hex_number = hex(number)[2:].upper()
+    # Convert to binary
+    binary_number = bin(number)[2:]
+
+    print(f"DECIMAL:{number}  HEXADECIMAL:{hex_number}  BINARY:{binary_number}")
+
